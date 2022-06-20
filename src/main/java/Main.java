ca3eeb88d1ffbc691a218e596d4683b1f79c0618
olgaobraztsova <olga.obraztsova@gmail.com>
2022-06-20 17:15:20 +0530

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        //демонстрация
+
+        System.out.println(bins.mult("1010", "1010"));
+
+        System.out.println(bins.sum("1010", "1010"));
+
+    }
+}
