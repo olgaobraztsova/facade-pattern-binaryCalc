2672711b14782e33ddb90f5fe734e767c94dbcfe
olgaobraztsova <olga.obraztsova@gmail.com>
2022-06-21 12:36:35 +0530

    fixed encoding

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BinOps bins = new BinOps();
-        //демонстрация
+        //РґРµРјРѕРЅСЃС‚СЂР°С†РёСЏ
 
         System.out.println(bins.mult("1010", "1010"));
 
